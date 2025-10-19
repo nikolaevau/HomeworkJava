4527a937b94161df65264f5551a774334c3df9c1
Ulyana Nikolaeva <u.nikolaeva@bft.ru>
2025-10-19 23:11:29 +0300

    Домашнее задание по модулю 3

---
+++ src/Homework3/Class2.java
@@ -0,0 +1,12 @@
+package Homework3;
+
+public class Class2 {
+    public static void main (String[] args) {
+        Class1 car = new Class1();
+
+        car.model = "Sportage";
+        car.year = 2021;
+
+        car.printInfo();
+    }
+}
